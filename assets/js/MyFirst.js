f69072f0ff6229c8657207a0db16ef48e2435619
Sergey <sv.sandp@gmail.com>
2021-03-07 15:27:34 +0200

    first commit

---
+++ assets/js/MyFirst.js
@@ -0,0 +1,75 @@
+
+'use stric';
+
+// var number = 3;
+// var string ="Hello";
+// var Boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// let something;
+// console.log(something)
+
+// let person = {
+// 	name: 'John',
+// 	age: 25,
+// 	isMarried: false
+
+// };
+
+//  console.log(person["name"]);
+
+// let arr = ['plum.png','orange.jpj','apple.bmp'];
+
+// console.log(arr[2]); 
+
+// // alert('Hello World!');
+
+// let answer = +prompt('How old are you?', 'yes');
+	
+
+// console.log(typeof(arr));
+// console.log(arr[0])
+// console.log(typeof(answer));
+
+// console.log("arr"+"-object");
+// console.log(4+ +"-object");
+
+
+
+
+// var incr = 10,
+// 	decr = 10;
+
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log("2" ====);
+
+// let money = prompt("Ваш бюджет на месяц?", ''),
+// 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+// let appData = {
+// 	budget: money,
+// 	expenses: {},
+// 	optionalExpenses: {},
+// 	income: [],
+// 	timeData: time,
+// 	savings: false
+// };
+
+// let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 	a2 = prompt("Во сколько обойдется?", ''),
+// 	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 	a4 = prompt("Во сколько обойдется?", '');
+
+// appData.expenses.a1 = a2;
+// appData.expenses.a3 = a4;
+
+// alert(appData.budget / 30);
