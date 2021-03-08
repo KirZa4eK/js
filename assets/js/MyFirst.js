77c6c6f1400ec48e9b55a0aa2ead52bb3baf4c7a
Sergey <sv.sandp@gmail.com>
2021-03-08 10:45:40 +0200

    changes in JS file

--- assets/js/MyFirst.js
+++ assets/js/MyFirst.js
@@ -75,9 +75,21 @@ for (let i = 0; i < 2; i++) {
     if ( (typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null && a !='' && b !='' && a.length <50) {
         console.log("done");
         appData.expenses[a] = b;
+    } else {
+
     }
     
 }
 
-alert(appData.budget / 30);
+appData.moneyPerDay = appData.budget /30;
+
+alert("Ежидневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+}
 
