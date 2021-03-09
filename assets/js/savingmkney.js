df089e7a399da3d009fc20925739d4361d6735a1
Sergey <sv.sandp@gmail.com>
2021-03-09 11:40:09 +0200

    add new js file

--- assets/js/savingmkney.js
+++ assets/js/savingmkney.js
@@ -1,3 +1,25 @@
-'use strict'
+function first() {
+    // Some actions
+    setTimeout( function(){
+        console.log(1, "1.");
 
-let money, per
+    }, 500 );
+}
+
+function second(){
+    console.log("4.");
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log("3. i just finished 2.13 lesson1 " + lang);
+    callback();
+}
+
+function done() {
+    console.log("2. i just finished lesseon2.13")
+}
+
+learnJS("Abracadabra", done);
